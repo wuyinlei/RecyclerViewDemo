6e1b500ea36268a549c8668da960f708d36b6679
wuyinlei <wuyinlei19931118@hotmail.com>
2016-09-20 17:48:26 +0800

    项目搭建

---
+++ app/src/main/java/novel/id/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package novel.id.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
